c91d42e7d70cfca48ec8a80eec4edb5ae3559464
mahmood1996 <mahmood.abdelrazek@outlook.com>
2022-03-13 00:35:31 +0200

    aggregate-assesment

---
+++ FrontDesk/src/FrontDesk.Api/Hubs/AppointmentDeletedHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FrontDesk.Core.Events;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FrontDesk.Api.Hubs
+{
+  public class AppointmentDeletedHandler : INotificationHandler<AppointmentDeletedEvent>
+  {
+    private readonly IHubContext<ScheduleHub> _hubContext;
+
+    public AppointmentDeletedHandler(IHubContext<ScheduleHub> hubContext)
+    {
+      _hubContext = hubContext;
+    }
+
+    public Task Handle(AppointmentDeletedEvent args, CancellationToken cancellationToken)
+    {
+      return _hubContext.Clients.All.SendAsync("ReceiveMessage", args.appointmentDeleted.Title + " was deleted", cancellationToken);
+    }
+  }
+}
---
+++ FrontDesk/src/FrontDesk.Core/Events/AppointmentDeletedEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FrontDesk.Core.ScheduleAggregate;
+using PluralsightDdd.SharedKernel;
+
+namespace FrontDesk.Core.Events
+{
+  public class AppointmentDeletedEvent : BaseDomainEvent
+  {
+    public AppointmentDeletedEvent(Appointment appointment)
+    {
+      appointmentDeleted = appointment;
+      Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; private set; }
+
+    public Appointment appointmentDeleted { get; private set;}
+  }
+}
